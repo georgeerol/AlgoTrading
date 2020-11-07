967d3fa42a52b2543cce760bb1df1d9759ce77a4
georgeerol <fouliex@gmail.com>
2020-11-07 15:27:45 -0800

    Working on charting and filtering

--- src/tradingapp/create_db.py
+++ src/tradingapp/create_db.py
@@ -1,4 +1,5 @@
 import sqlite3
+
 connection = sqlite3.connect('app.db')
 cursor = connection.cursor()
 
@@ -6,7 +7,8 @@ cursor.execute("""
     CREATE TABLE IF NOT EXISTS stock (
         id INTEGER PRIMARY KEY, 
         symbol TEXT NOT NULL UNIQUE, 
-        name TEXT NOT NULL
+        name TEXT NOT NULL,
+        exchange TEXT NOT NULL 
     )
 """)
 cursor.execute("""
@@ -22,6 +24,27 @@ cursor.execute("""
         FOREIGN KEY (stock_id) REFERENCES stock (id)
     )
 """)
-#cursor.execute("INSERT INTO stock (symbol, company) VALUES ('ADBE', 'Adobe Inc.')")
-#cursor.execute("INSERT INTO stock (symbol, company) VALUES ('VZ', 'Verizon')")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS strategy (
+        id INTEGER PRIMARY KEY,
+        name NOT NULL
+    )
+""")
+cursor.execute("""
+  CREATE TABLE IF NOT EXISTS stock_strategy(
+      stock_id INTEGER NOT NULL,
+      strategy_id INTEGER NOT NULL,
+      FOREIGN KEY (stock_id) REFERENCES stock (id)
+      FOREIGN KEY (strategy_id) REFERENCES strategy(id)
+  )
+""")
+strategies = ['opening_range_breakout', 'opening_range_breakdown']
+
+for strategy in strategies:
+    cursor.execute("""
+        INSERT INTO strategy (name) VALUES (?)
+    """, (strategy,))
+# cursor.execute("INSERT INTO stock (symbol, company) VALUES ('ADBE', 'Adobe Inc.')")
+# cursor.execute("INSERT INTO stock (symbol, company) VALUES ('VZ', 'Verizon')")
 connection.commit()
--- src/tradingapp/main.py
+++ src/tradingapp/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from src.tradingapp import config
+from datetime import date
 import sqlite3
 import uvicorn
 
@@ -10,13 +11,25 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def index(request: Request):
+    stock_filter = request.query_params.get('filter', False)
     connection = sqlite3.connect(config.DB_FILE)
     connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
-    cursor.execute("""
-        SELECT id, symbol, name FROM stock
-        ORDER BY symbol;
-    """)
+
+    if stock_filter == 'new_closing_highs':
+        cursor.execute("""
+        select * from (
+        select symbol,name,stock_id,max(close),date
+        from stock_price join stock on stock.id = stock_price.stock_id
+        group by stock_id
+        order by symbol
+        ) where date =?
+        """, (date.today().isoformat(),))
+    else:
+        cursor.execute("""
+            SELECT id, symbol, name FROM stock
+            ORDER BY symbol;
+        """)
     rows = cursor.fetchall()
     return templates.TemplateResponse("index.html", {"request": request, "stocks": rows})
 
@@ -26,6 +39,11 @@ def stock_detail(request: Request, symbol):
     connection = sqlite3.connect(config.DB_FILE)
     connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT * FROM strategy
+    """)
+    strategies = cursor.fetchall()
     cursor.execute("""
         SELECT id, symbol, name FROM stock WHERE symbol = ?;
     """, (symbol,))
@@ -37,7 +55,7 @@ def stock_detail(request: Request, symbol):
     """, (row['id'],))
 
     bars = cursor.fetchall()
-    return templates.TemplateResponse("stock_detail.html", {"request": request, "stock": row,"bars": bars})
+    return templates.TemplateResponse("stock_detail.html", {"request": request, "stock": row, "bars": bars,"strategies":strategies})
 
 
 if __name__ == "__main__":
