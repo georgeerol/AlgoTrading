deb8b2ebba82af57e60c565338d56b7d706a9f9e
georgeerol <fouliex@gmail.com>
2020-11-08 13:08:29 -0800

    Add charting and filtering

--- src/tradingapp/main.py
+++ src/tradingapp/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from src.tradingapp import config
 from datetime import date
@@ -55,7 +56,46 @@ def stock_detail(request: Request, symbol):
     """, (row['id'],))
 
     bars = cursor.fetchall()
-    return templates.TemplateResponse("stock_detail.html", {"request": request, "stock": row, "bars": bars,"strategies":strategies})
+    return templates.TemplateResponse("stock_detail.html",
+                                      {"request": request, "stock": row, "bars": bars, "strategies": strategies})
+
+
+@app.post("/apply_strategy")
+def apply_strategy(strategy_id: int = Form(...), stock_id: int = Form(...)):
+    connection = sqlite3.connect(config.DB_FILE)
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        INSERT INTO stock_strategy (stock_id, strategy_id) VALUES (?,?)
+    """, (stock_id, strategy_id))
+
+    connection.commit()
+    return RedirectResponse(url=f"/strategy/{strategy_id}", status_code=303)
+
+
+@app.get("/strategy/{strategy_id}")
+def strategy(request: Request, strategy_id):
+    connection = sqlite3.connect(config.DB_FILE)
+    connection.row_factory = sqlite3.Row
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT id, name
+        FROM strategy
+        WHERE id = ?
+    """, (strategy_id,))
+
+    strategy = cursor.fetchone()
+
+    cursor.execute("""
+        SELECT symbol, name
+        FROM stock JOIN stock_strategy on stock_strategy.stock_id = stock.id
+        WHERE strategy_id = ?
+    """, (strategy_id,))
+
+    stocks = cursor.fetchall()
+    return templates.TemplateResponse("strategy.html", {"request": request, "stocks": stocks, "strategy": strategy})
 
 
 if __name__ == "__main__":
